ae48365f41bf338a0569b075dabbcbbd9b024931
Ben Norris <ben@bsn.io>
2016-02-12 07:18:12 -0700

    Initial blank framework

---
+++ EnvironmentSwitcher/EnvironmentSwitcher.h
@@ -0,0 +1,19 @@
+//
+//  EnvironmentSwitcher.h
+//  EnvironmentSwitcher
+//
+//  Created by Ben Norris on 2/12/16.
+//  Copyright © 2016 OC Tanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EnvironmentSwitcher.
+FOUNDATION_EXPORT double EnvironmentSwitcherVersionNumber;
+
+//! Project version string for EnvironmentSwitcher.
+FOUNDATION_EXPORT const unsigned char EnvironmentSwitcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EnvironmentSwitcher/PublicHeader.h>
+
+
